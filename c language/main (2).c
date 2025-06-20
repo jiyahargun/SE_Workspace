6eb0c523b5e3514ac2f754ba03965bea29a7215a
jiyahargun <jiyahargun82@gmail.com>
2025-06-20 10:53:11 +0530

    Add files via upload

---
+++ c language/main (2).c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    printf("\n Your Name : jiya hargun");
+    printf("\n Your Age : 20");
+    printf("\n Your City : jetpur");
+    printf("\n your DOB : 08-02-2005");
+    
+    getch();
+}
+                          
