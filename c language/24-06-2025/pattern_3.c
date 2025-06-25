8ef82804524f06aa8cbe1e0f35ced3add6e34afc
jiyahargun <jiyahargun82@gmail.com>
2025-06-25 11:49:05 +0530

    Add files via upload

---
+++ c language/24-06-2025/pattern_3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j,num;
+    
+    printf("Enter Any Number");
+    scanf("%d",&num);
+    
+    for(i=1;i<=num;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            
+            printf("%d",j);
+        }
+        printf("\n");
+         
+    }
+
+
+    return 0;
+}
---
+++ c language/24-06-2025/pattern_5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j,num;
+    
+    printf("Enter Any Number");
+    scanf("%d",&num);
+    
+    for(i=0;i<=num;i++)
+    {
+        for(j=0;j<=i;j++)
+        {
+           printf("%c",'A'+i); 
+        }
+        printf("\n");
+         
+    }
+    
+
+    return 0;
+}
